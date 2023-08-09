286f9ae4f3b459dca2059b7502230b1bb7343881
sandinius <santiagoandin@gmail.com>
2023-08-09 17:38:17 -0300

    cambios para la preentrega 2

--- js/main.js
+++ js/main.js
@@ -1,65 +1,32 @@
-function sueldoneto() {
-    let retenciones = 17;
-    const porcentaje = (sueldoBruto, descuento) => (sueldoBruto * descuento)/100;
-    while (true){
-        let sueldo = Number(prompt('Introduzca el sueldo Bruto: '));
-        if (sueldo > 0){
-            bruto = sueldo - porcentaje(sueldo, retenciones);
-            alert(`Este es el sueldo Neto: ${bruto}`);
-            break
-        }else{
-            alert('Introduzca un numero mayor que 0');
-        }
-    }
+
+const productos = [
+    { producto: "fideos", precio: 2000 },
+  { producto: "manteca", precio: 600 },
+  { producto: "leche", precio: 1000 },
+  { producto: "puerro", precio: 1200 },
+  { producto: "albaca", precio: 300 },
+];
+
+const carrito = [];
+
+const sacarPrecio = (comprado) => {
+    const buscaPrecios = productos.find(i => i.producto === comprado);
+    return buscaPrecios ? buscaPrecios.precio : "El producto no se encuentra disponible";
+};
+
+let producto = prompt('Estos son nuestros productos disponibles: fideos, manteca, leche, puerro y albaca. Escribe "fin" para finalizar.');
+
+while (producto.toLowerCase() !== 'fin') {
+    let valor = sacarPrecio(producto);
+    
+    carrito.push({ producto: producto, precio: valor });
+    
+    producto = prompt('Estos son nuestros productos disponibles: fideos, manteca, leche, puerro y albaca. Escribe "fin" para finalizar.');
 }
-
-function jubilacion() {
-    let genero = prompt('Escriba H si es hombre o M si es mujer. si ya termino escriba fin.');
-    let edad = Number(prompt('Escriba su edad'));
-    while (genero != 'fin'){
-
-        switch(genero){
-            case "H":
-                anos = 65 - edad;
-                alert(`Le quedan ${anos} años para jubilarse`);
-                break
-            case "h":
-                anos = 65 - edad;
-                alert(`Le quedan ${anos} años para jubilarse`);
-                break
-            case "M":
-                anos = 60 - edad;
-                alert(`Le quedan ${anos} años para jubilarse`);
-                break
-            case "m":
-                anos = 60 - edad;
-                alert(`Le quedan ${anos} años para jubilarse`);
-                break
-            default:
-                alert('Escriba una de las opciones');
-                break
-        }
-        genero = prompt('Escriba H si es hombre o M si es mujer. si ya termino escriba fin.');
-        if (genero != 'fin'){
-        edad = Number(prompt('Escriba su edad'));
-        }
-    }
+let mensajeCarrito = 'Carrito:\n';
+for (const item of carrito) {
+    mensajeCarrito += `Producto: ${item.producto}, Precio: ${item.precio}\n`;
 }
 
 
-let calculo = prompt('Si desea calcular los años que le quedan para jubilarse escriba "jubilacion" o si desea calcular su sueldo escriba "sueldo neto". Si no desea calcular nada mas escriba fin.');
- 
-while(calculo != 'fin'){
-    switch(calculo){
-        case ('sueldo neto'):
-            sueldoneto();
-            break;
-        case ('jubilacion'):
-            jubilacion()
-            break;
-        default:
-            alert('Ponga una de las opciones');
-            break;
-        }
-    calculo = prompt('Si desea calcular los años que le quedan para jubilarse escriba "jubilacion" o si desea calcular su sueldo escriba "sueldo neto". Si no desea calcular nada mas escriba fin.');
-    }
+alert(`Este es su carrito ${mensajeCarrito}`)
